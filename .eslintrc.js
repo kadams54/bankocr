abd66af9fc988ef632a1e6d8b3f3fc7990269b96
Kyle Adams <kadams54@gmail.com>
2017-07-14 08:01:58 -0400

    Clean test data, refine setup.

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
     indent: ['error', 2],
     'linebreak-style': ['error', 'unix'],
     quotes: ['error', 'single'],
-    semi: ['error', 'always']
+    semi: ['error', 'always'],
+    'no-console': 0
   }
 };
